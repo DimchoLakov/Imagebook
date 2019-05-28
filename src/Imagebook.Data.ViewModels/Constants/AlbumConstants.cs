5041598b0f070a5cb34508e26050f492ea0e7775
DimchoLakov <mdsektor@gmail.com>
2019-05-28 20:38:05 +0100

    Added AlbumsConstants.cs for ViewModels

--- src/Imagebook.Data.ViewModels/Constants/AlbumConstants.cs
+++ src/Imagebook.Data.ViewModels/Constants/AlbumConstants.cs
@@ -3,5 +3,15 @@
     public class AlbumConstants
     {
         public const string DateTimeFormat = "dddd, MMMM d, yyyy";
+
+        public const string Name = "Album Name";
+
+        public const string CreatedOn = "Created On";
+
+        public const string Description = "Decription";
+
+        public const string Username = "Username";
+
+        public const string Location = "Location";
     }
 }

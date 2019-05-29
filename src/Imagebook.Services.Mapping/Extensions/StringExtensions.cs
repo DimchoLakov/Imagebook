6a4f82e8012583ee427d3dce2a1631ed67537bd5
DimchoLakov <mdsektor@gmail.com>
2019-05-29 20:27:31 +0100

    Added StringExtensions class

---
+++ src/Imagebook.Services.Mapping/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Imagebook.Services.Mapping.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetBase64(this byte[] imageBytes)
+        {
+            var base64 = Convert.ToBase64String(imageBytes);
+            var imageSrc = $"data:image/jpg;base64,{base64}";
+
+            return imageSrc;
+        }
+    }
+}

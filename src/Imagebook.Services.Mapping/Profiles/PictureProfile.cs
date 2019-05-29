dffc02d9dfc64de1929274f8f774c966c54cccf2
DimchoLakov <mdsektor@gmail.com>
2019-05-29 20:27:44 +0100

    Added Picture Profile (Automapper)

---
+++ src/Imagebook.Services.Mapping/Profiles/PictureProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Imagebook.Data.Models;
+using Imagebook.Data.ViewModels.Pictures;
+using Imagebook.Services.Mapping.Extensions;
+
+namespace Imagebook.Services.Mapping.Profiles
+{
+    public class PictureProfile : Profile
+    {
+        public PictureProfile()
+        {
+            this.CreateMap<Picture, PictureViewModel>()
+                .ForMember(dest => dest.Src, from => from.MapFrom(src => src.ImageArray.GetBase64()))
+                .ReverseMap();
+        }
+    }
+}

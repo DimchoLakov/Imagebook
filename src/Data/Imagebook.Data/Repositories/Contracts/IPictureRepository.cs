d75362115671c261d34a6cfdbcd62e6f87d40a43
DimchoLakov <mdsektor@gmail.com>
2019-05-29 20:25:34 +0100

    Added Picture Repository

---
+++ src/Data/Imagebook.Data/Repositories/Contracts/IPictureRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Imagebook.Data.Models;
+
+namespace Imagebook.Data.Repositories.Contracts
+{
+    public interface IPictureRepository : IRepository<Picture>
+    {
+        Task<IQueryable<Picture>> AllByAlbumId(string albumId);
+    }
+}
---
+++ src/Data/Imagebook.Data/Repositories/PictureRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Imagebook.Data.Models;
+using Imagebook.Data.Repositories.Contracts;
+
+namespace Imagebook.Data.Repositories
+{
+    public class PictureRepository : GenericRepository<Picture>, IPictureRepository
+    {
+        public PictureRepository(ImagebookDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IQueryable<Picture>> AllByAlbumId(string albumId)
+        {
+            var allPictures = await this.AllAsync();
+            return allPictures.Where(p => p.AlbumId == albumId);
+        }
+    }
+}

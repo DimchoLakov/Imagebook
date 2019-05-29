5ff4b27e1282450c6b6a3db52910bf41d960e20d
DimchoLakov <mdsektor@gmail.com>
2019-05-29 20:28:42 +0100

    Implemented Unit of Work

---
+++ src/Data/Imagebook.Data/UnitOfWork/Contracts/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Imagebook.Data.Repositories.Contracts;
+
+namespace Imagebook.Data.UnitOfWork.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IAlbumRepository Albums { get; }
+
+        IPictureRepository Pictures { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/Data/Imagebook.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Imagebook.Data.Repositories;
+using Imagebook.Data.Repositories.Contracts;
+using Imagebook.Data.UnitOfWork.Contracts;
+
+namespace Imagebook.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ImagebookDbContext _dbContext;
+
+        public UnitOfWork(ImagebookDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+            this.Albums = new AlbumRepository(dbContext);
+            this.Pictures = new PictureRepository(dbContext);
+        }
+
+        public IAlbumRepository Albums { get; }
+
+        public IPictureRepository Pictures { get; }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return this._dbContext.SaveChangesAsync();
+        }
+    }
+}
